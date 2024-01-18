abde83d8c592d709d13a2994e0b2e307e6fb1f5c
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-01-18 18:07:47 +0100

    Added cors.

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"hotel-back-v1/db"
 	"hotel-back-v1/routes"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +14,15 @@ func main() {
 	db.InitDB()
 
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.RegisterRoutes(server)
 
